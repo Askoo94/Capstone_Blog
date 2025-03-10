21f935575a4d1076306983e595e34583af2f4871
Ascareel KICAVANGA <ascareel94@gmail.com>
2025-03-10 21:21:39 +0100

    Add the routing

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("index.ejs");
+});
+
+app.get('/', (req, res) => {
+  res.render("index.ejs");
 });
 
 app.listen(port, () => {
