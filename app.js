de3fc85127467b19fdb1a56965e271408b5751c7
Ascareel KICAVANGA <ascareel94@gmail.com>
2025-03-11 18:46:40 +0100

    Add submit for the blog

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(express.static('public'));
 
@@ -9,8 +13,14 @@ app.get('/', (req, res) => {
   res.render("index.ejs");
 });
 
-app.get('/', (req, res) => {
-  res.render("index.ejs");
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", {
+    Prénom : req.body["nom"], 
+    Pays : req.body["country"],
+    Ville : req.body["city"],
+    Image : req.body["image"],
+    Histoire : req.body["histoire"]
+  });
 });
 
 app.listen(port, () => {
