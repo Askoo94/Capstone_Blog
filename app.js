fdaedca4e260fb3797c54b10fe4c25a6141b3191
Ascareel KICAVANGA <ascareel94@gmail.com>
2025-03-16 21:30:18 +0100

    Add the function to get the data in blog table

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //Enable body parse in the body
 
+app.use(express.static('public')); // Enable the distribution of static file
 
-app.use(express.static('public'));
+const readDB = () => JSON.parse(fs.readFileSync('./db.json', 'utf8')); // Object that read the entire database (JSON file)
+
+// Get the Name in Database (JSON file)
+const getUserNameById = (id) => {
+  const db = readDB(); // db est un objet contenant { users: [...] }
+  const blog = db.blog.find(blog => blog.id === id);
+  return blog ? blog.name : "Utilisateur non trouvé";
+};
+
+// Get the Countryin Database (JSON file)
+const getUserCountryById = (id) => {
+  const db = readDB(); // db est un objet contenant { users: [...] }
+  const blog = db.blog.find(blog => blog.id === id);
+  return blog ? blog.country : "Utilisateur non trouvé";
+};
+
+// Get the City in Database (JSON file)
+const getUserCityById = (id) => {
+  const db = readDB(); // db est un objet contenant { users: [...] }
+  const blog = db.blog.find(blog => blog.id === id);
+  return blog ? blog.city : "Utilisateur non trouvé";
+};
+
+// Get the Image in Database (JSON file)
+const getUserImageById = (id) => {
+  const db = readDB(); // db est un objet contenant { users: [...] }
+  const blog = db.blog.find(blog => blog.id === id);
+  return blog ? blog.image: "Utilisateur non trouvé";
+};
+
+// Get the Story in Database (JSON file)
+const getUserStoryById = (id) => {
+  const db = readDB(); // db est un objet contenant { users: [...] }
+  const blog = db.blog.find(blog => blog.id === id);
+  return blog ? blog.story : "Utilisateur non trouvé";
+};
+
+//console.log(getUserStoryById(1));
 
 let year = new Date();
 
