99c4b6e0fd902bc0a9eb81fadbbe6cf3a3dd62d3
Ascareel KICAVANGA <ascareel94@gmail.com>
2025-03-22 11:24:23 +0100

    Implement the db.json data in the views and change dynamic date in footer

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ import fs from "fs";
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true })); //Enable body parse in the body
+app.use(bodyParser.urlencoded({ extended: true })); // Enable body parse in the body
 
 app.use(express.static('public')); // Enable the distribution of static file
 
-const readDB = () => JSON.parse(fs.readFileSync('./db.json', 'utf8')); // Object that read the entire database (JSON file)
+const jsonData = () => fs.readFileSync('./db.json', 'utf8'); // Object that read the entire database (JSON file)
+const data = JSON.parse(jsonData()) // Convert JSON -> JS Object
+
+let year = new Date();
+
+//console.log(data.blog)
 
 // Get the Name in Database (JSON file)
+/*
 const getUserNameById = (id) => {
   const db = readDB(); // db est un objet contenant { users: [...] }
   const blog = db.blog.find(blog => blog.id === id);
@@ -44,26 +50,27 @@ const getUserStoryById = (id) => {
   const db = readDB(); // db est un objet contenant { users: [...] }
   const blog = db.blog.find(blog => blog.id === id);
   return blog ? blog.story : "Utilisateur non trouvé";
-};
+}; */
 
 //console.log(getUserStoryById(1));
 
-let year = new Date();
+
+
 
 app.get('/', (req, res) => {
   res.render("index.ejs", {
-    date : year.getFullYear()
+    blogData : data
   });
 });
 
 app.post("/submit", (req, res) => {
   res.render("index.ejs", {
+    blogData : data,
     Prénom : req.body["nom"], 
     Pays : req.body["country"],
     Ville : req.body["city"],
     Image : req.body["image"],
-    Histoire : req.body["histoire"],
-    date : year.getFullYear()
+    Histoire : req.body["histoire"]
   });
 });
 
