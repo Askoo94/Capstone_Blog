caa07a9b8e4c06c4e95b2a6b340159427691cc92
Ascareel KICAVANGA <ascareel94@gmail.com>
2025-03-30 21:10:15 +0200

    Resolve the form event problem

--- public/script/script.js
+++ public/script/script.js
@@ -5,33 +5,41 @@ $("#mySelect").on("change", function(event) {
 
     let value = selectElement[0].value;
 
-    console.log(selectElement[0].value);
-    console.log("J'aime manger !");
+    //console.log(selectElement[0].value);
 
     switch (value) {
 
         // Make a post
         case "1":
-            $("#form1").show();
-            $("#form3").hide();
-            $("#form2").hide();
+            $("form").attr("action", "/submit");
+            $("#image").prop("disabled", false);
+            $("#story").prop("disabled", false);
+
             break;
 
         // Modify a post
         case "2":
-            $("#form3").show();
-            $("#form1").hide();
-            $("#form2").hide();
+            $("form").attr("action", "/put");
+            $("#image").prop("disabled", false);
+            $("#story").prop("disabled", false);
+
             break;
 
         // Delete a post  
         case "3":
-            $("#form1").hide();
-            $("#form3").hide();
-            $("#form2").show();
+            $("form").attr("action", "/delete");
+            $("#image").prop("disabled", true);
+            $("#story").prop("disabled", true);
+
             break;    
     
         default:
             break;
-    }
+    } 
 });
+
+
+
+
+
+
